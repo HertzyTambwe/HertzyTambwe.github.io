44175e79d0e26ec040510ee87caa5b5c59b993ce
HertzyTambwe <hertytambwe@gmail.com>
2025-02-20 08:57:13 +0200

    implementation de index

--- my_page/my_page/urls.py
+++ my_page/my_page/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 
+from portfolio.views import index
+
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
